9572ed6b828e7df8a79665f08df6225f71712639
Rohan Adhikari <itsrohanadhikari@gmail.com>
2026-06-30 03:44:56 +0530

    Time: 16 ms (20.71%) | Memory: 35.1 MB (19.48%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findDays(vector<int> weights, int mid) {
+    int findDays(vector<int>& weights, int mid) {
         int cnt = 0;
         int i = 0;
         int temp = mid;
@@ -12,24 +12,21 @@ public:
             temp -= weights[i];
             i++;
         }
-        return cnt+1;
+        return cnt + 1;
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int e = accumulate(weights.begin(), weights.end(), 0);
-        int s= *max_element(weights.begin(),weights.end());
+        int s = *max_element(weights.begin(), weights.end());
         int mid;
-        int ans = e;
         while (s <= e) {
             mid = s + (e - s) / 2;
             int total = findDays(weights, mid);
-            if (total <= days && mid>=s) {
-                cout << total << " " << mid << endl;
-                ans = min(ans, mid);
+            if (total <= days && mid >= s) {
                 e = mid - 1;
             } else {
                 s = mid + 1;
             }
         }
-        return ans;
+        return s;
     }
 };
