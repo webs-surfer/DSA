9d13d407ffc3d1066ea20bc69306906b43bd4275
Rohan Adhikari <itsrohanadhikari@gmail.com>
2026-07-14 19:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (80.43%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -6,50 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        if (root == NULL)
-            return {};
-        stack<TreeNode*> st;
+        if (root == NULL) return {};
         queue<TreeNode*> q;
+        vector<vector<int>> ans;
+        vector<int> tempAns;
         q.push(root);
         q.push(NULL);
         while (!q.empty()) {
             TreeNode* temp = q.front();
-            st.push(q.front());
             q.pop();
             if (temp == NULL) {
+                ans.push_back(tempAns);
+                tempAns.clear();
                 if (!q.empty()) {
                     q.push(NULL);
                 }
             } else {
-                if (temp->right) {
-                    q.push(temp->right);
-                }
+                tempAns.push_back(temp->val);
                 if (temp->left) {
                     q.push(temp->left);
                 }
+                if (temp->right) {
+                    q.push(temp->right);
+                }
             }
         }
-        st.pop();
-        vector<int> temp;
-        vector<vector<int>> res;
-        while (!st.empty()) {
-            if (st.top()) {
-                temp.push_back(st.top()->val);
-            } else {
-                res.push_back(temp);
-                temp.clear();
-            }
-            st.pop();
-        }
-        if (!temp.empty())
-            res.push_back(temp);
-        return res;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
